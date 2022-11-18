897d7ec4ac4dd4ab881568e1474071edb1014755
kaiyu li <krisfk@gmail.com>
2022-11-18 19:24:55 +0800

    .

--- wp-content/themes/storefront-child/page-test2.php
+++ wp-content/themes/storefront-child/page-test2.php
@@ -55,45 +55,45 @@ ini_set("display_errors", "On"); // 設定是否顯示錯誤( On=顯示, Off=隱
 
 
 
-// echo 22;
+
 if(!empty($_FILES))
 {
-    echo 11;
-    //File 1
+
     if($_FILES["file_upload"]['size'])
     {
             $wordpress_upload_dir = wp_upload_dir();
 
-            $new_file_path = $wordpress_upload_dir['path'] . '/' . $_FILES["file_upload"]["name"];
+            print_r($_FILES);
+            // $new_file_path = $wordpress_upload_dir['path'] . '/' . $_FILES["file_upload"]["name"];
 
-                $i=0;
-            while( file_exists( $new_file_path ) ) {
-                $i++;
-                $new_file_path = $wordpress_upload_dir['path'] . '/' . $i . '_' . $_FILES["file_upload"]["name"];
-            }
+            //     $i=0;
+            // while( file_exists( $new_file_path ) ) {
+            //     $i++;
+            //     $new_file_path = $wordpress_upload_dir['path'] . '/' . $i . '_' . $_FILES["file_upload"]["name"];
+            // }
             
-            if (move_uploaded_file($_FILES["file_upload"]["tmp_name"], $new_file_path)) {
+            // if (move_uploaded_file($_FILES["file_upload"]["tmp_name"], $new_file_path)) {
 
-                $upload_id = wp_insert_attachment( array(
-                'guid'           => $new_file_path, 
-                'post_mime_type' => 'image/*',
-                //$_FILES["file_upload"]["tmp_name"],
-                'post_title'     => preg_replace( '/\.[^.]+$/', '', $_FILES["file_upload"]["name"] ),
-                'post_content'   => '',
-                'post_status'    => 'inherit'
-            ), $new_file_path );
-            require_once( ABSPATH . 'wp-admin/includes/image.php' );
+            //     $upload_id = wp_insert_attachment( array(
+            //     'guid'           => $new_file_path, 
+            //     'post_mime_type' => 'image/*',
+            //     //$_FILES["file_upload"]["tmp_name"],
+            //     'post_title'     => preg_replace( '/\.[^.]+$/', '', $_FILES["file_upload"]["name"] ),
+            //     'post_content'   => '',
+            //     'post_status'    => 'inherit'
+            // ), $new_file_path );
+            // require_once( ABSPATH . 'wp-admin/includes/image.php' );
 
-            wp_update_attachment_metadata( $upload_id, wp_generate_attachment_metadata( $upload_id, $new_file_path ) );
-            //  conclude Image
+            // wp_update_attachment_metadata( $upload_id, wp_generate_attachment_metadata( $upload_id, $new_file_path ) );
+            // //  conclude Image
 
-            update_field( 'conclude_image', $upload_id, $school_id );
+            // update_field( 'conclude_image', $upload_id, $school_id );
             
-            // conclusion_content
+            // // conclusion_content
         
-            } else {
-                echo "Sorry, there was an error uploading your file.";
-            }        
+            // } else {
+            //     echo "Sorry, there was an error uploading your file.";
+            // }        
     }
 
     echo $new_file_path;
