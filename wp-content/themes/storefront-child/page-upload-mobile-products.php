10f2e3fe0648b0041185607f4354852bb51a9962
kaiyu li <krisfk@gmail.com>
2022-11-19 02:38:57 +0800

    .

--- wp-content/themes/storefront-child/page-upload-mobile-products.php
+++ wp-content/themes/storefront-child/page-upload-mobile-products.php
@@ -32,6 +32,7 @@ $data = [
             'id' => 14
         ]
     ],
+    'manage_stock' => false,
     'images' => [
         [
             'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg'
