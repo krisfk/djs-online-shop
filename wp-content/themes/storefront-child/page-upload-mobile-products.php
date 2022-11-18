e357430aab31293b8b619cad7926b49e455dc447
kaiyu li <krisfk@gmail.com>
2022-11-19 04:18:16 +0800

    .

--- wp-content/themes/storefront-child/page-upload-mobile-products.php
+++ wp-content/themes/storefront-child/page-upload-mobile-products.php
@@ -157,7 +157,7 @@ if($_POST['submit'])
             </div>
 
             <div class="mt-3">
-                <label class="label-bold" for="product-description">Product Description:(如用default文字留返一點 "." 係度)</label>
+                <label class="label-bold" for="product-description">Product Description:(如用default文字留返一點係度)</label>
                 <textarea type="text" class="form-control" name="product-description" rows="4" cols="50"
                     id="product-description">.</textarea>
             </div>
