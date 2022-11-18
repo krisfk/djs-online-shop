da471d0e56ba0d315b88be8485685a8637afe27a
kaiyu li <krisfk@gmail.com>
2022-11-18 18:22:41 +0800

    .

--- upload-product-testing.php
+++ upload-product-testing.php
@@ -2,31 +2,34 @@
 
 require_once('./wp-load.php');
 
+ini_set("display_errors", "On"); // 設定是否顯示錯誤( On=顯示, Off=隱藏 )
 
-// $data = [
-//     'name' => 'Premium Quality',
-//     'type' => 'simple',
-//     'regular_price' => '21.99',
-//     'description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.',
-//     'short_description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-//     'categories' => [
-//         [
-//             'id' => 9
-//         ],
-//         [
-//             'id' => 14
-//         ]
-//     ],
-//     'images' => [
-//         [
-//             'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg'
-//         ],
-//         [
-//             'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg'
-//         ]
-//     ]
-// ];
 
-// print_r($woocommerce->post('products', $data));
+
+$data = [
+    'name' => 'Premium Quality',
+    'type' => 'simple',
+    'regular_price' => '21.99',
+    'description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.',
+    'short_description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+    'categories' => [
+        [
+            'id' => 9
+        ],
+        [
+            'id' => 14
+        ]
+    ],
+    'images' => [
+        [
+            'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg'
+        ],
+        [
+            'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg'
+        ]
+    ]
+];
+
+print_r($woocommerce->post('products', $data));
 
 ?>
