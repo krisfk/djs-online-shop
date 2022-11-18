2e54d1b6d2d98cdc84394889b223816d8693ef73
kaiyu li <krisfk@gmail.com>
2022-11-19 05:56:35 +0800

    .

--- upload-product-testing.php
+++ upload-product-testing.php
@@ -6,44 +6,44 @@ require_once('./wp-load.php');
 ini_set("display_errors", "On"); // 設定是否顯示錯誤( On=顯示, Off=隱藏 )
 
 
-require __DIR__ . '/vendor/autoload.php';
+// require __DIR__ . '/vendor/autoload.php';
 
-use Automattic\WooCommerce\Client;
+// use Automattic\WooCommerce\Client;
 
-$woocommerce = new Client(
-  'https://www.djs.com.hk',
-  'ck_de8ae56fb0542de4713462233af0d4a727fce173',
-  'cs_792f6090a1f222378942329064bfa10e386b3f4a',
-  [
-    'version' => 'wc/v3',
-  ]
-);
+// $woocommerce = new Client(
+//   'https://www.djs.com.hk',
+//   'ck_de8ae56fb0542de4713462233af0d4a727fce173',
+//   'cs_792f6090a1f222378942329064bfa10e386b3f4a',
+//   [
+//     'version' => 'wc/v3',
+//   ]
+// );
 
-$data = [
-    'name' => 'Premium Quality',
-    'type' => 'simple',
-    'regular_price' => '21.99',
-    'description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.',
-    'short_description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-    'categories' => [
-        [
-            'id' => 9
-        ],
-        [
-            'id' => 14
-        ]
-    ],
-    'images' => [
-        [
-            'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg'
-        ],
-        [
-            'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg'
-        ]
-    ]
-];
+// $data = [
+//     'name' => 'Premium Quality',
+//     'type' => 'simple',
+//     'regular_price' => '21.99',
+//     'description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.',
+//     'short_description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+//     'categories' => [
+//         [
+//             'id' => 9
+//         ],
+//         [
+//             'id' => 14
+//         ]
+//     ],
+//     'images' => [
+//         [
+//             'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg'
+//         ],
+//         [
+//             'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg'
+//         ]
+//     ]
+// ];
 
-$woocommerce->post('products', $data);
+// $woocommerce->post('products', $data);
 
 ?>
 
