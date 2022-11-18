330a2d12961359e0362cb4b4933abf31143c3cf2
kaiyu li <krisfk@gmail.com>
2022-11-18 18:16:48 +0800

    .

--- upload-product-testing.php
+++ upload-product-testing.php
@@ -26,7 +26,7 @@ require_once('./wp-load.php');
 //echo $min;
 
 //echo 9099;
-ini_set("display_errors", "On"); // 設定是否顯示錯誤( On=顯示, Off=隱藏 )
+// ini_set("display_errors", "On"); // 設定是否顯示錯誤( On=顯示, Off=隱藏 )
 
 // 
 
