1722789024b4ea8dd47cdb095b51e0f9c7c5412d
kaiyu li <krisfk@gmail.com>
2022-11-19 02:37:00 +0800

    .

--- wp-content/themes/storefront-child/page-upload-mobile-products.php
+++ wp-content/themes/storefront-child/page-upload-mobile-products.php
@@ -17,64 +17,64 @@ $woocommerce = new Client(
 );
 
 
-//simple
-// $data = [
-//     'name' => 'Premium Quality',
-//     'type' => 'simple',
-//     'regular_price' => '21.99',
-//     'description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.',
-//     'short_description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-//     'categories' => [
-//         [
-//             'id' => 9
-//         ],
-//         [
-//             'id' => 14
-//         ]
-//     ],
-//     'images' => [
-//         [
-//             'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg'
-//         ],
-//         [
-//             'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg'
-//         ]
-//     ]
-// ];
+// simple
+$data = [
+    'name' => 'Premium Quality',
+    'type' => 'simple',
+    'regular_price' => '21.99',
+    'description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.',
+    'short_description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+    'categories' => [
+        [
+            'id' => 9
+        ],
+        [
+            'id' => 14
+        ]
+    ],
+    'images' => [
+        [
+            'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg'
+        ],
+        [
+            'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg'
+        ]
+    ]
+];
 
-// $woocommerce->post('products', $data);
+$woocommerce->post('products', $data);
 
 
 
 //variation
 
 
-$prod_data = [
-	'name'        => 'A great product',
-	'type'        => 'variable',
-	'description' => 'A very meaningful product description',
-	'images'      => [
-		[
-			'src'      => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg',
-			'position' => 0,
-		],
-	],
-	'categories'  => [
-		[
-			'id' => 1,
-		],
-	],
-	'attributes'  => [
-		[
-			'id'        => 5,
-			'variation' => true,
-			'visible'   => true,
-			'options'   => [ 'S', 'M', 'L' ],
-		],
-	],
-];
+// $prod_data = [
+// 	'name'        => 'A great product',
+// 	'type'        => 'variable',
+// 	'description' => 'A very meaningful product description',
+// 	'images'      => [
+// 		[
+// 			'src'      => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg',
+// 			'position' => 0,
+// 		],
+// 	],
+// 	'categories'  => [
+// 		[
+// 			'id' => 1,
+// 		],
+// 	],
+// 	'attributes'  => [
+// 		[
+// 			'id'        => 5,
+// 			'variation' => true,
+// 			'visible'   => true,
+// 			'options'   => [ 'S', 'M', 'L' ],
+// 		],
+// 	],
+// ];
 
-$woocommerce->post( 'products', $prod_data );
+// $woocommerce->post( 'products', $prod_data );
 
 
 ?>
