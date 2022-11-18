d5b0ba7e1533bd69bbd0c676dec18c25bf635e15
kaiyu li <krisfk@gmail.com>
2022-11-18 19:14:48 +0800

    .

--- wp-content/themes/storefront-child/page-test2.php
+++ wp-content/themes/storefront-child/page-test2.php
@@ -46,7 +46,8 @@ ini_set("display_errors", "On"); // 設定是否顯示錯誤( On=顯示, Off=隱
 
 
 <form action="" method="post" enctype="multipart/form-data">
-    <input type="file" accept="image/*" multiple>
+    <!-- <input type="file" accept="image/*" multiple> -->
+    <input type="file" name="file_upload" id="file_upload" accept=".jpg">
     <input type="submit" value="submit">
 </form>
 <?php
