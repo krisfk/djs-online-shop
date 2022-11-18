6ef01b40226e08f01148a84b62fff28fda80bfab
kaiyu li <krisfk@gmail.com>
2022-11-19 02:28:20 +0800

    .

--- wp-content/themes/storefront-child/page-upload-mobile-products.php
+++ wp-content/themes/storefront-child/page-upload-mobile-products.php
@@ -59,10 +59,13 @@ ini_set("display_errors", "On"); // 設定是否顯示錯誤( On=顯示, Off=隱
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
     </script>
+    <link rel="stylesheet" href="<?php echo get_site_url();?>/product-upload.css">
+
 </head>
 
 <body>
 
+    <div class="container"></div>
 </body>
 
 </html>
