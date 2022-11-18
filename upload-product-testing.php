668d848a84b1c98a7ffadce4caa253d0b6f8e1a0
kaiyu li <krisfk@gmail.com>
2022-11-18 18:21:42 +0800

    .

--- upload-product-testing.php
+++ upload-product-testing.php
@@ -2,91 +2,31 @@
 
 require_once('./wp-load.php');
 
-// $args = array(
-// 	'post_type' => 'product',
-// 	'post_status' => 'publish',
-// 	'posts_per_page' => 1 ,
-// 	'orderby'=> 'menu_order',
-// 	'order' => 'ASC',
-// );
 
-// $products = new WP_Query( $args );
-// $products->the_post();
-// $smallest_menu_order =  get_post_field( 'menu_order', get_the_ID());
-
-// echo 2;
-
-
-//echo $products->found_posts;
-
-//echo do_shortcode('[products id="all" orderby="menu_order"]');
-
-//
-//$min = $wpdb->get_var('SELECT MIN(menu_order) AS min FROM '.$wpdb->prefix.'postmeta AS pm, " .$wpdb->prefix.'posts AS po WHERE pm.post_id = po.ID AND po.post_status = "publish" AND po.post_type = "products");
-//echo $min;
-
-//echo 9099;
-// ini_set("display_errors", "On"); // 設定是否顯示錯誤( On=顯示, Off=隱藏 )
-
-// 
-
-// Load Composer autoloader.
-// @link https://github.com/brightnucleus/jasper-client/blob/master/tests/bootstrap.php#L55-L59
-// $autoloader = dirname( __FILE__ ) . '/vendor/autoload.php';
-// if ( is_readable( $autoloader ) ) {
-// 	require_once $autoloader;
-// }
-
-use Automattic\WooCommerce\Client;
-
-
-//WC_API_Client
-//http://www.kayli.me/djs-online-shop/wc-api/v3/products?filter[limit]=-1
-$woocommerce = new Client(
-	'http://www.djs.com.hk',
-	'ck_5652ff4d3946a2318721aaff55cc71b0d35841c2',
-	'cs_4faf2b6d95dfa1e1655790bb266a4f2fcbb19cfe',
-	[
-		'wp_api'  => true,
-		'version' => 'wc/v3',
-	]
-);
-
-// $products = $woocommerce->get('products',array('per_page'=>20,'show_in_rest'=>true));
-//echo sizeof($products);
-//print_r($products);
-//
-// $data = [
-//     'name' => 'Color',
-//     'slug' => 'pa_color',
-//     'type' => 'select',
-//     'order_by' => 'menu_order',
-//     'has_archives' => true
-// ];
-
-//print_r($woocommerce->get('products/attributes/'));
-
-
-$prod_data = [
-	'name'          => 'A great product',
-	'type'          => 'simple',
-	'regular_price' => '15.00',
-	'description'   => 'A very meaningful product description',
-	'images'        => [
-		[
-			'src'      => 'https://shop.local/path/to/image.jpg',
-			'position' => 0,
-		],
-	],
-	'categories'    => [
-		[
-			'id' => 1,
-		],
-	],
+$data = [
+    'name' => 'Premium Quality',
+    'type' => 'simple',
+    'regular_price' => '21.99',
+    'description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.',
+    'short_description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+    'categories' => [
+        [
+            'id' => 9
+        ],
+        [
+            'id' => 14
+        ]
+    ],
+    'images' => [
+        [
+            'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg'
+        ],
+        [
+            'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg'
+        ]
+    ]
 ];
 
-$woocommerce->post( 'products', $prod_data );
+print_r($woocommerce->post('products', $data));
 
-
-
- ?>
+?>
