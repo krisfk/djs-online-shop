4d703d1652a363ee9bbf0db5f1231758b2cf4d53
kaiyu li <krisfk@gmail.com>
2022-11-19 04:45:12 +0800

    .

--- upload-product-testing.php
+++ upload-product-testing.php
@@ -57,7 +57,7 @@ if(!empty($_FILES))
 
     $total = count($_FILES['files_upload']['name']);
 
-    // echo $total;
+    echo $total;
     // print_r($_FILES);
     // print_r()
     // if($_FILES["files_upload"]['size'])
